215774ec1574f0d7249a36a71dfd65cb6201bf2d
Aryan <aryanbiswari14@gmail.com>
2023-01-07 20:13:34 +0530

    contest complete

--- script.js
+++ script.js
@@ -8,20 +8,43 @@ let arr = [
 
 function PrintDeveloperswithMap() {
   //Write your code here
+  arr
+  .map((employee) => {
+    if (employee.profession === 'developer') {
+      console.log(employee);
+    }
+  });
 }
 
 function PrintDeveloperbyForEach() {
   //Write your code here
+  arr.forEach((employee) => {
+    if (employee.profession === 'developer') {
+      console.log(employee);
+    }
+  });
 }
 
 function addData() {
   //Write your code here
+  const newEmployee = {id:4,name:"susan",age:"20",profession:"intern"};
+  arr.push(newEmployee);
+  console.log(arr);
 }
 
 function removeAdmin() {
   //Write your code here
+  arr = arr.filter((employee) => employee.profession !== 'admin');
+  console.log(arr);
 }
 
 function concatenateArray() {
   //Write your code here
+  const newEmployees = [
+    {id:4,name:"susan",age:"20",profession:"intern"},
+    {id:5,name:"bob",age:"21",profession:"developer"},
+    {id:6,name:"kate",age:"22",profession:"admin"}
+  ];
+  const combinedEmployees = arr.concat(newEmployees);
+  console.log(combinedEmployees);
 }
